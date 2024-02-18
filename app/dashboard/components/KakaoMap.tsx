fbdd9838441fb1d4bef4dad58ad65a304b26c4d0
no-pla <songjihyun323@gmail.com>
2024-02-19 02:17:50 +0900

    feat: 대시보드 접속 시 현재 위치 보여주는 기능 추가

--- app/dashboard/components/KakaoMap.tsx
+++ app/dashboard/components/KakaoMap.tsx
@@ -1,12 +1,56 @@
 "use client";
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 import { Map } from "react-kakao-maps-sdk";
 
 const KakaoMap = () => {
+  const [state, setState] = useState<any>({
+    center: {
+      lat: 33.450701,
+      lng: 126.570667,
+    },
+    errMsg: null,
+    isLoading: true,
+  });
+
+  useEffect(() => {
+    // 지도 띄울 때 현재 위치로 고정
+    // TODO: 메인 화면 병원 클릭해서 들어왔을 때는 작동하지 않도록 처리해야 함.
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setState((prev: any) => ({
+            ...prev,
+            center: {
+              lat: position.coords.latitude, // 위도
+              lng: position.coords.longitude, // 경도
+            },
+            isLoading: false,
+          }));
+        },
+        (err) => {
+          setState((prev: any) => ({
+            ...prev,
+            errMsg: err.message,
+            isLoading: false,
+          }));
+        }
+      );
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+      setState((prev: any) => ({
+        ...prev,
+        errMsg: "geolocation을 사용할수 없어요..",
+        isLoading: false,
+      }));
+    }
+  }, []);
+
   return (
     <Map
       id="map"
-      center={{ lat: 33.5563, lng: 126.79581 }}
+      center={state.center}
       style={{ width: "100vw", height: "100vh" }}
     ></Map>
   );
