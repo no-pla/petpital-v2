a5f1cc2b892ab889abf89c158248068480e40c80
no-pla <songjihyun323@gmail.com>
2024-08-30 12:48:53 +0900

    feat: add Kakao OAuth Sign In #1

--- share/authOptions.ts
+++ share/authOptions.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { NextAuthOptions } from "next-auth";
 import NaverProvider from "next-auth/providers/naver";
+import KakaoProvider from "next-auth/providers/kakao";
 
 const prisma = new PrismaClient();
 
@@ -11,29 +12,50 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.NAVER_CLIENT_SECRET!,
       allowDangerousEmailAccountLinking: true,
     }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
   ],
   callbacks: {
-    async signIn({ user, profile, credentials, account }) {
-      if (!credentials) {
-        if (account?.provider === "naver") {
-          const userExist = await prisma.user.findUnique({
-            where: {
-              email: profile?.response.email,
+    async signIn({ user, profile, account }) {
+      if (account?.provider === "naver") {
+        const userExist = await prisma.user.findUnique({
+          where: {
+            email: profile?.response?.email,
+          },
+        });
+
+        if (!userExist) {
+          const newUser = await prisma.user.create({
+            data: {
+              email: profile?.response?.email,
+              image: profile?.response?.profile_image,
+              name: profile?.response?.nickname,
             },
           });
+          user.id = newUser.id;
+        } else {
+          user = Object.assign(user, userExist);
+        }
+      } else if (account?.provider === "kakao") {
+        const userExist = await prisma.user.findUnique({
+          where: {
+            email: profile?.kakao_account?.email,
+          },
+        });
 
-          if (!userExist) {
-            const newUser = await prisma.user.create({
-              data: {
-                email: profile?.response!.email!,
-                image: profile?.response!.profile_image!,
-                name: profile?.response!.nickname!,
-              },
-            });
-            user.id = newUser.id;
-          } else {
-            user = Object.assign(user, userExist);
-          }
+        if (!userExist) {
+          const newUser = await prisma.user.create({
+            data: {
+              email: profile?.kakao_account?.email,
+              image: profile?.properties?.profile_image,
+              name: profile?.properties?.nickname,
+            },
+          });
+          user.id = newUser.id;
+        } else {
+          user = Object.assign(user, userExist);
         }
       }
       return true;
