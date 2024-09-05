1a3ad7b976bede092a7637d22b92213656a36560
no-pla <songjihyun323@gmail.com>
2024-09-06 02:16:20 +0900

    feat: Added Naver interceptor to fix the format issue with expires_in in Naver OAuth within Next-Auth v5. #1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,21 @@
-import { handlers } from "@/auth";
-export const { GET, POST } = handlers;
+import { GET as AuthGET, POST as AuthPOST } from "@/auth";
+import { naverFetchInterceptor } from "@/interceptor/naver-interceptor";
+import { type NextRequest } from "next/server";
+
+const originalFetch = fetch;
+
+export async function POST(req: NextRequest) {
+  return await AuthPOST(req);
+}
+
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+
+  if (url.pathname === "/api/auth/callback/naver") {
+    global.fetch = naverFetchInterceptor(originalFetch);
+    const response = await AuthGET(req);
+    global.fetch = originalFetch;
+    return response;
+  }
+  return await AuthGET(req);
+}
--- auth.ts
+++ auth.ts
@@ -1,7 +1,18 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import Naver from "next-auth/providers/naver";
+import { prisma } from "./prisma";
+import { Provider } from "next-auth/providers";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
+export const CustomNaverAuthProvider: Provider = Naver({
+  clientId: process.env.AUTH_NAVER_ID,
+  clientSecret: process.env.AUTH_NAVER_SECRET,
+  authorization: "https://nid.naver.com/oauth2.0/authorize?response_type=code",
 });
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [CustomNaverAuthProvider],
+});
+
+export const { GET, POST } = handlers;
